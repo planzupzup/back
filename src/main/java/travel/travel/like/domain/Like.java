989765bdaa686633341d96500e76718752d0473c
Huiyeongkim <huiyeong9619@naver.com>
2025-06-26 17:42:07 +0900

    refactor : 좋아요는 댓글에 적용되도록 수정

--- src/main/java/travel/travel/like/domain/Like.java
+++ src/main/java/travel/travel/like/domain/Like.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import travel.travel.comment.domain.Comment;
 import travel.travel.member.domain.Member;
 import travel.travel.plan.domain.Plan;
 
@@ -23,6 +24,6 @@ public class Like {
     private Member member;
 
     @ManyToOne
-    @JoinColumn(name = "plan_id")
-    private Plan plan;
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
 }
--- src/main/java/travel/travel/like/service/LikeService.java
+++ src/main/java/travel/travel/like/service/LikeService.java
@@ -4,11 +4,12 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import travel.travel.comment.domain.Comment;
+import travel.travel.comment.repository.CommentRepository;
 import travel.travel.like.domain.Like;
 import travel.travel.like.repository.LikeRepository;
 import travel.travel.member.domain.Member;
 import travel.travel.member.repository.MemberRepository;
-import travel.travel.plan.domain.Plan;
 import travel.travel.plan.repository.PlanRepository;
 
 import java.util.Optional;
@@ -19,21 +20,26 @@ import java.util.Optional;
 public class LikeService {
 
     private final LikeRepository likeRepository;
-    private final PlanRepository planRepository;
+    private final CommentRepository commentRepository;
     private final MemberRepository memberRepository;
+    private final PlanRepository planRepository;
 
-    public void addLike(Long planId) {
+    public void addLike(Long commentId) {
         String memberId = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Member member = memberRepository.findById(Long.valueOf(memberId))
                 .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다."));
 
-        Plan plan = planRepository.findById(planId)
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 댓글입니다."));
+
+        planRepository.findById(comment.getPlan().getPlanId())
                 .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 게시글입니다."));
-        Optional<Like> existingLike = likeRepository.findByMemberAndPlan(member, plan);
+
+        Optional<Like> existingLike = likeRepository.findByMemberAndComment(member, comment);
         if (existingLike.isEmpty()) {
             Like like = Like.builder()
                     .member(member)
-                    .plan(plan)
+                    .comment(comment)
                     .build();
             likeRepository.save(like);
         } else {
@@ -41,20 +47,28 @@ public class LikeService {
         }
     }
 
-    public void removeLike(Long planId) {
+    public void removeLike(Long commentId) {
         String memberId = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Member member = memberRepository.findById(Long.valueOf(memberId))
                 .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다."));
 
-        Plan plan = planRepository.findById(planId)
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 댓글입니다."));
+
+        planRepository.findById(comment.getPlan().getPlanId())
                 .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 게시글입니다."));
-        Optional<Like> existingLike = likeRepository.findByMemberAndPlan(member, plan);
+
+        Optional<Like> existingLike = likeRepository.findByMemberAndComment(member, comment);
         existingLike.ifPresent(likeRepository::delete);
     }
 
-    public long getLikeCount(Long planId) {
-        Plan plan = planRepository.findById(planId)
+    public long getLikeCount(Long commentId) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 댓글입니다."));
+
+        planRepository.findById(comment.getPlan().getPlanId())
                 .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 게시글입니다."));
-        return likeRepository.countByPlan(plan);
+
+        return likeRepository.countByComment(comment);
     }
 }
