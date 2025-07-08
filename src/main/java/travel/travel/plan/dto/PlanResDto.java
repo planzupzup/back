6c06452a7de2ef9d078066ea353b0dd13bd1d877
Huiyeongkim <huiyeong9619@naver.com>
2025-07-08 20:25:40 +0900

    refactor: plan response dto에 닉네임 추가

--- src/main/java/travel/travel/plan/dto/PlanResDto.java
+++ src/main/java/travel/travel/plan/dto/PlanResDto.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Builder
 public class PlanResDto {
     private Long planId;
+    private String nickName;
     private String title;
     private String content;
     private LocalDate startDate;
@@ -26,6 +27,7 @@ public class PlanResDto {
     public static PlanResDto of(Plan plan, List<LocationThumbResDto> locations) {
         return PlanResDto.builder()
                 .planId(plan.getPlanId())
+                .nickName(plan.getMember().getNickName())
                 .title(plan.getTitle())
                 .content(plan.getContent())
                 .startDate(plan.getStartDate())
