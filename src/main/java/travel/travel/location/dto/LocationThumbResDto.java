57e1fc34905a5bb041f3d7ce2104f5e76dc01234
Huiyeongkim <huiyeong9619@naver.com>
2025-07-24 22:26:05 +0900

    refactor: 지역 썸네일응답 순서 변경

--- src/main/java/travel/travel/location/dto/LocationThumbResDto.java
+++ src/main/java/travel/travel/location/dto/LocationThumbResDto.java
@@ -13,13 +13,12 @@ public class LocationThumbResDto {
 
     private Long locationId;
     private String locationName;
+    private LocalDate day;
+    private Integer scheduleOrder;
 
     private double latitude;
     private double longitude;
     private String address;
-    private LocalDate day;
-
-    private Integer scheduleOrder;
 
     private String placeId;
     private String googleImageUrl;
