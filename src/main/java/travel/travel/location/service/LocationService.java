efa5a1566a64ed047511ca0fc0f3cd3c7c12eb2d
Huiyeongkim <huiyeong9619@naver.com>
2025-07-26 18:42:17 +0900

    refactor: day 요청 DTO에서 삭제

--- src/main/java/travel/travel/location/service/LocationService.java
+++ src/main/java/travel/travel/location/service/LocationService.java
@@ -63,30 +63,31 @@ public class LocationService {
     public List<List<LocationResDto>> createLocation(List<List<LocationCreateReqDto>> locationCreateReqDtoList, Long planId) {
         Plan findPlan = planRepository.findById(planId)
                 .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 계획입니다."));
-
+        locationRepository.deleteLocationsByPlan(findPlan);
         List<List<LocationResDto>> result = new ArrayList<>();
 
+        Integer day = 1;
         for (List<LocationCreateReqDto> dayLocationList : locationCreateReqDtoList) {
             List<LocationResDto> dayResult = new ArrayList<>();
-
             int order = 0;
             for (LocationCreateReqDto locationDto : dayLocationList) {
 
-                validateDay(locationDto, findPlan);
+                validateDay(findPlan, day);
                 Location saved = locationRepository.save(
-                        LocationCreateReqDto.toEntity(locationDto, findPlan, order + 1));
+                        LocationCreateReqDto.toEntity(locationDto, findPlan, day,order + 1));
 
                 dayResult.add(LocationResDto.of(saved));
                 order++;
             }
+            day ++;
             result.add(dayResult);
         }
         return result;
     }
 
-    private void validateDay(LocationCreateReqDto locationDto, Plan plan) {
+    private void validateDay(Plan plan, Integer day) {
         long totalDays = ChronoUnit.DAYS.between(plan.getStartDate(), plan.getEndDate()) + 1;
-        if (locationDto.getDay() < 1 || locationDto.getDay() > totalDays) {
+        if (day < 1 || day > totalDays) {
             throw new IllegalArgumentException("요청하신 day 값이 계획 범위를 벗어났습니다.");
         }
     }
