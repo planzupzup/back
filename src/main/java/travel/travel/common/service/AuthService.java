1914b1dca39229f7cad5acb0fbe4ca534d7724f2
Huiyeongkim <huiyeong9619@naver.com>
2025-07-27 16:11:58 +0900

    feat: 현재 인증된 사용자 ID 조회 기능 추가

--- src/main/java/travel/travel/common/service/AuthService.java
+++ src/main/java/travel/travel/common/service/AuthService.java
@@ -1,17 +1,17 @@
 package travel.travel.common.service;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class AuthService {
 
     public Long getAuthenticatedUserId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if(!isAuthenticatedUser()) throw new EntityNotFoundException("존재하지 않는 회원입니다.");
-        return Long.parseLong(authentication.getName());
+        if(!isAuthenticatedUser()) throw new SecurityException("존재하지 않는 회원입니다.");
+        return Long.parseLong(authentication.getPrincipal().toString());
     }
 
     public boolean isAuthenticatedUser() {
