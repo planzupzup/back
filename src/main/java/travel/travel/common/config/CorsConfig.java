dff7545d4a0e17f4e4c412005cc93c75a3e35d4a
Huiyeongkim <huiyeong9619@naver.com>
2025-08-06 16:25:18 +0900

    feat: 로그인 cors 추가

--- src/main/java/travel/travel/common/config/CorsConfig.java
+++ src/main/java/travel/travel/common/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://localhost:3000", "https://planzupzup.vercel.app"));
+        configuration.setAllowedOrigins(List.of("https://localhost:3000", "https://planzupzup.vercel.app", "https://planzupzup.co.kr"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
