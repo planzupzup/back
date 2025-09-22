290e201419d5ddbfe101023e3ee27aefec3029de
Huiyeongkim <huiyeong9619@naver.com>
2025-09-22 16:52:28 +0900

    feat: swagger config 추가

--- src/main/java/travel/travel/common/config/CorsConfig.java
+++ src/main/java/travel/travel/common/config/CorsConfig.java
@@ -15,8 +15,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://localhost:3000", "https://planzupzup.vercel.app", "https://planzupzup.co.kr"));
-        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedOrigins(List.of("https://localhost:3000", "http://localhost:8080", "https://planzupzup.vercel.app", "https://planzupzup.co.kr"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
